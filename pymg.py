6342ac89ea9dd80aae896bf49f8b0e6708e988c2
Haz Matts <hazmat@drinkitover.net>
2014-02-02 21:21:30 -0800

    Pulling all comments now working, but pretty slow.

--- pymg.py
+++ pymg.py
@@ -2,13 +2,15 @@
 import sys
 import requests
 import getpass
+import time
+import random
 from bs4 import BeautifulSoup
 
 # Pull the name/pwd from CLI args/prompt
 # The getpass lib pulls the library without it going into command history
 # or being displayed on the screen. Strip anything weird.
 imun  = sys.argv[1].strip()
-impwd = getpass.getpass("imgur pwd: ").strip()
+#impwd = getpass.getpass("imgur pwd: ").strip()
 
 # Debugging proxies
 prx = { 'http': 'http://192.168.1.213:8080', 'https': 'http://192.168.1.213:8080' }
@@ -25,37 +27,34 @@ headers = {
    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.76 Safari/537.36',
    'Referer': 'https://imgur.com/include/signin-iframe.html'
 }
+cmnts = []
 
 # login will submit the username/password, validate a successful auth response, and return the session object.
 # The session object will maintain cookies/auth status across requests.
+session = requests.Session()
 
 def login():
 	pdata = {'username': imun, 'password': impwd, 'remember': 'remember', 'submit_form': 'Sign+in' }
 	# Create a session object to snag cookies and authenticate.
-	s = requests.Session()
 	try:
 		#login = s.post('https://'+host+'/signin?redirect=http://imgur.com/', data=pdata, headers=headers, allow_redirects=False)
-		login = s.post('https://'+host+'/signin?redirect=http://imgur.com/', data=pdata, headers=headers, proxies=prx, verify=False, allow_redirects=False)
+		login = session.post('https://'+host+'/signin?redirect=http://imgur.com/', data=pdata, headers=headers, proxies=prx, verify=False, allow_redirects=False)
 	except:
 		print 'Some shit be bad.'
 		print login
 	if login.status_code == 302:
 		#return dict(s.cookies.get_dict())
-		return s
+		return True
 	else:
 		print 'fail omg'
 		print login.status_code
 		exit(2)
-
-# Begin the session and authenticate.
-# Remove the Referer header, hoping requests will take care of this
-session = login()
-del headers['Referer']
+	# Remove the Referer header, hoping requests will take care of this
+	del headers['Referer']
 
 # The 'sid' value is passed on up/down votes. Slurp it out of the soup.
 # Create the list for comment IDs
 #sid = soup.find('input' id='sid')['value']
-cmnts = []
 
 def get_profile(target):
 	# Hit the profile page of our target and return the soup object.
@@ -68,7 +67,7 @@ def get_profile(target):
 		soup = BeautifulSoup(prof.text)
 		return soup, soup.find('span', class_='stat').text
 
-def get_comments(soup):
+def extract_comments(soup):
 	# Append all of the comment IDs to the cmnts list and return how many
 	# were found. This lets us know when we reach the last page.
 	found = 0
@@ -77,10 +76,30 @@ def get_comments(soup):
 		found += 1
 	return found
 
-def page_comments(page, target):
-	# Comments are returned 20 per page, so submit a request for the next page.
-	soup = BeautifulSoup(s.get('http://'+host+'/user/'+target+'/index/newest/page/'+page+'?scrolling'))
-	return soup
+def get_all_comments(target):
+	getmore = True
+	page = 0
+	rsptimes = []
+	while getmore == True:
+		req = session.get('http://'+host+'/user/'+target+'/index/newest/page/'+str(page))
+		rsptimes.append(req.elapsed)
+		csoup = BeautifulSoup(req.text)
+		fetched = extract_comments(csoup)
+		page += 1
+		print "[%d comments collected]" % (len(cmnts))
+		if fetched != 20:
+			getmore = False
+			print "Last fetch returned %d comments. Last page reached." % fetched
+			print str(len(cmnts)) + " total comments."
+			break
+		if page % 10 == 0:
+			sleeptime = random.uniform(0.203, 0.522)
+			print "Sleeping for %.3f." % sleeptime
+			time.sleep(sleeptime)
+	print "Response Times"
+	print rsptimes
+	print "Average Response Times:"
+	print sum(rsptimes) / float(len(rsptimes))
 
-user = get_profile(sys.argv[2])
-print user[1]
+
+get_all_comments(sys.argv[2])
