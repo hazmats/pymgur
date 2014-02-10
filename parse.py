489bbd33c3d8a7a3eb93993b9a0b28b88ac9c50b
hazmat <twitch@urmom.(none)>
2014-02-10 00:27:47 -0800

    Parsing stuff is fun

--- parse.py
+++ parse.py
@@ -3,4 +3,10 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser(description='Do some imgr')
-parser.add_argument('-t', '--target', help='Target username')
+parser.add_argument('source', help='Source username from which to submit votes.')
+parser.add_argument('target', help='Target username')
+parser.add_argument('action', help='Accepts up, down, or veto.')
+
+args = parser.parse_args()
+
+print "Targeting %s with %s votes from account %s" % (args.target, args.action, args.source)
