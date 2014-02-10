1e181c004b8c9fbd006ba29762cb0af6b85df700
hazmat <twitch@urmom.(none)>
2014-02-10 12:20:31 -0800

    Accepting some args. Fetch profile only now working.

--- pymg.py
+++ pymg.py
@@ -8,19 +8,28 @@ from bs4 import BeautifulSoup
 
 # Begin arg parsing with argparse.
 parser = argparse.ArgumentParser(description='Do some imgr')
-parser.add_argument('source', help='Source username from which to submit votes.', default='qazZAQQ')
-parser.add_argument('target', help='Target username')
-parser.add_argument('action', help='Accepts up, down, or veto.')
-parser.add_argument('type', help='Accepts "image" or "caption"', default="caption")
+parser.add_argument('-s', '--source', help='Source username from which to submit votes.')
+parser.add_argument('-a', '--action', help='Accepts up, down, or veto.')
+parser.add_argument('--type', help='Accepts "image" or "caption"', default="all")
+parser.add_argument('-f', '--file', help='File containing csv of usernames/passwords to use.')
+parser.add_argument('-l', '--log', help='Track and save information about this session and user to a file.')
+parser.add_argument('--prof', help="Don't login, just fetch user profile data.", action='store_true')
+parser.add_argument('-t', '--target', help='Target username', required=True)
 args = parser.parse_args()
 
 
 # Debugging proxies
 prx = { 'http': 'http://192.168.1.213:8080', 'https': 'http://192.168.1.213:8080' }
 
-# Define some params
+#####################################
+###   Declaractions and globals   ###
+#####################################
+
 # host so we can switch to localhost for testing
 # pdata is our POST data
+# cmnts is a list of collected comments
+# ctxt is the text of these comments
+# I still feel like there *must* be a better way...
 host = 'imgur.com'
 cmnts = []
 ctxt = {}
@@ -29,7 +38,7 @@ ctxt = {}
 # The session object will maintain cookies/auth status across requests.
 session = requests.Session()
 
-# A list of User-Agent headers we can pull randomly from
+# A list of User-Agent headers from which we can pull randomly
 userAgents = [
 	'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36',
 	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36',
@@ -68,10 +77,15 @@ headers = {
    'User-Agent': random.choice(userAgents),
    'Referer': 'https://imgur.com/include/signin-iframe.html'
 }
+
+#######################
+###   Functions!!   ###
+#######################
+
 def login(srcuser):
 	"""
 	Authenticates existing session object with host using provided username and password.
-	Expects no input.
+	Expects username with which to login and interactive prompt for pwd
 	Returns None.
 	"""
 	impwd = getpass.getpass("imgur pwd: ").strip()
@@ -81,15 +95,14 @@ def login(srcuser):
 		login = session.post('https://'+host+'/signin?redirect=http://imgur.com/', data=pdata, headers=headers, allow_redirects=False)
 		#login = session.post('https://'+host+'/signin?redirect=http://imgur.com/', data=pdata, headers=headers, proxies=prx, verify=False, allow_redirects=False)
 	except:
-		print 'Some shit be bad.'
+		print 'Some shit be bad. Failed login request'
 	if login.status_code == 302:
-		#return dict(s.cookies.get_dict())
 		return True
 	else:
-		print 'fail omg'
+		print 'auth fail omg'
 		print login.status_code
 		exit(2)
-	# Remove the Referer header, hoping requests will take care of this
+	# Remove the Referer header, requests will take care of this
 	del headers['Referer']
 
 
@@ -99,7 +112,8 @@ def get_profile(target):
 	Expects the username to be passed in as 'target'
 	Returns a list of the BS profile soup and current reputation.
 	"""
-	prof = session.get('http://'+host+'/user/'+target, headers=headers, proxies=prx)
+	#prof = session.get('http://'+host+'/user/'+target, headers=headers, proxies=prx)
+	prof = session.get('http://'+host+'/user/'+target, headers=headers)
 	if prof.status_code == 404:
 		print "User %s not found.\nExiting." % target
 		exit(2)
@@ -116,7 +130,7 @@ def extract_comments(soup):
 	found = 0
 	for x in soup.find_all('div', class_='caption'):
 		cmnts.append(x['data'])
-		ctxt[x['data'] = x.find_all('div', class_='usertext textbox first1')[0].find_all('span')[2].string.strip()
+		ctxt[x['data']] = x.find_all('div', class_='usertext textbox first1')[0].find_all('span')[2].string.strip()
 		found += 1
 	return found
 
@@ -136,6 +150,10 @@ def get_all_comments(target):
 			print "Last fetch returned %d comments. Last page reached." % fetched
 			print str(len(cmnts)) + " total comments."
 			break
+		# DEBUGGING CODE - REMOVE ME
+		if page >= 3:
+			getmore = False
+			break
 	# The 'sid' value is passed on up/down votes. Slurp it out of the soup.
 	# Create the list for comment IDs
 	global sid 
@@ -151,16 +169,25 @@ def vote(commid, action, itype, reps=len(cmnts)):
 	testcmnts = True
 	maxcmnts = 0
 	#to use later when done testing: while maxcmnts != reps:
-	for comment in commid:
+	# DEBUGGING CODE -- REMOVE ME (the slice)
+	for comment in commid[0:4]:
 		#dv = session.post('http://'+host+'/gallery/action/vote/'+comment, data=pdata, proxies=prx)
 		dv = session.post('http://'+host+'/gallery/action/vote/'+comment, data=pdata)
-		print "Submitted for..."
-		print comment
-		print dv.elapsed
+		print comment + " " + action + " " + dv.elapsed
+
+
+if args.prof:
+	print "Current reputation for user %s is %s" % (args.target, get_profile(args.target)[1])
+	exit()
 
 print "Logging in..."
 login(args.source)
 print "Logged in."
-get_all_comments(args.target)
-print "Comments fetched. Now %svoting..." % args.action
+if args.type == "caption":
+	get_all_comments(args.target)
+	print "Comments fetched. Now %svoting..." % args.action
+else:
+	print "No code for images yet, dumbass. Go write it."
+	exit(2)
+
 vote(cmnts, args.action, 'caption')
